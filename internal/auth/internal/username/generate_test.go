6f101e660b61d5e535947b74e69b6e9271d02e80
Lumi <lumi4x@proton.me>
2025-10-10 12:59:13 -0300

    undo

--- internal/auth/internal/username/generate_test.go
+++ internal/auth/internal/username/generate_test.go
@@ -8,7 +8,7 @@ import (
 // TestGenerateUsername generates usernames a significant amount to ensure there's no panic
 func TestGenerateUsername(t *testing.T) {
 	for i := 0; i < 10000; i++ {
-		username := GenerateUsername()
+		username := Generate()
 		slog.Info("Generated username successfully", "username", username)
 	}
 }
